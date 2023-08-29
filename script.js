844fc7e35274b4f4fea87412bdceed68c3b9e094
Hubertoom <h.michalec@o2.pl>
2023-08-29 14:14:46 +0200

    added animation to notification

--- script.js
+++ script.js
@@ -1,82 +1,107 @@
-const inputUrl = document.getElementById("input-url");
-const buttonCreate = document.getElementById("button-create");
-const buttonDelete = document.getElementById("button-delete");
-const listUrl = document.getElementById("list-url");
-const invalidUrl = document.getElementById("invalid-url");
-
-buttonCreate.addEventListener("click", () => {
-    const url = inputUrl.value;
-    if (isValidUrl(url)) {
-        invalidUrl.style.display = "none";
-        let shortUrl = generateShortUrl();
-        const link = document.createElement("a");
-        link.href = url;
-        link.target = "_blank";
-        link.textContent = shortUrl;
-        const clickCount = document.createElement("span");
-        clickCount.textContent = "Clicks: 0";
-        const buttonEdit = document.createElement("button");
-        buttonEdit.id = "button-edit";
-        buttonEdit.textContent = "Edit";
-        const listItem = document.createElement("li");
-        listItem.appendChild(link);
-        listItem.appendChild(document.createTextNode(`   ${url} `));
-        listItem.appendChild(clickCount);
-        listItem.appendChild(buttonEdit);
-        listUrl.appendChild(listItem);
-
-        buttonEdit.addEventListener("click", () => {
-            const randomString = shortUrl.slice(-5);
-            const inputEdit = document.createElement("input");
-            inputEdit.type = "text";
-            inputEdit.value = randomString;
-            listItem.replaceChild(inputEdit, link);
-            buttonEdit.textContent = "Save";
-
-            buttonEdit.addEventListener("click", () => {
-                const newRandomString = inputEdit.value;
-                shortUrl = shortUrl.replace(randomString, newRandomString);
-                link.href = url;
-                link.textContent = shortUrl;
-                listItem.replaceChild(link, inputEdit);
-                buttonEdit.textContent = "Edit";
-            });
-        });
-
-        link.addEventListener("click", () => {
-            let count = parseInt(clickCount.textContent.split(": ")[1]);
-            count++;
-            clickCount.textContent = `Clicks: ${count}`;
-        });
-    } else {
-        invalidUrl.style.display = "block";
+const createButton = document.getElementById("button-create");
+const deleteButton = document.getElementById("button-delete");
+const urlField = document.getElementById("input-url");
+const list = document.getElementById("list-url");
+const notificationArea = document.getElementById("notification");
+let notification = document.getElementById('notification');
+function addItem() {
+    let url = urlField.value;
+    if (!isUrlValid(url)) {
+        notification.style.display = 'block';
+        return;
     }
-});
+    notification.style.display = 'none';
 
-buttonDelete.addEventListener("click", () => {
-    const input = inputUrl.value.trim();
-    if (input.length === 0) {
-        listUrl.innerHTML = "";
-    } else {
-        const items = listUrl.getElementsByTagName("li");
-        for (let i = items.length - 1; i >= 0; i--) {
-            const link = items[i].getElementsByTagName("a")[0];
-            if (link.href === input || link.textContent === input) {
-                listUrl.removeChild(items[i]);
-            }
-        }
-    }
-});
+    let listElement = document.createElement('li');
+    let linkShort = document.createElement('a');
+    let linkOriginal = document.createElement('a');
+    let text = document.createTextNode(' - ');
+    let shortUrl = generateShortUrl();
+
+
+    linkOriginal.href = url;
+    linkOriginal.textContent = url;
+    linkOriginal.target = '_blank';
+    linkOriginal.style.textDecoration = 'none';
+    linkOriginal.style.fontSize = '1rem';
+
+    linkShort.href = url;
+    linkShort.textContent = shortUrl;
+    linkShort.target = '_blank';
+    linkShort.style.textDecoration = 'none';
+    linkShort.style.fontSize = '1.5rem';
+
+    let counter = document.createElement('span');
+    let c = 0;
+    counter.textContent = ` Clicks: ${c}`;
+    linkShort.addEventListener('click', () => {
+        counter.textContent = ` Clicks: ${++c}`;
+    });
+
+    listElement.appendChild(linkShort);
+    listElement.appendChild(text);
+    listElement.appendChild(linkOriginal);
+    listElement.appendChild(counter);
+
+
+    let editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.className = "editButton";
+    let saveButton = document.createElement('button');
+    saveButton.textContent = "Save";
+    saveButton.className = "saveButton";
+
+    let inputEditedUrl = document.createElement('input');
+    inputEditedUrl.className = "inputEditedUrl";
+    inputEditedUrl.size = 10;
+
+    editButton.addEventListener('click', () => {
+        inputEditedUrl.value = linkShort.textContent.slice(-5);
+        linkShort.replaceWith(inputEditedUrl);
+        editButton.replaceWith(saveButton);
+    });
+
+    saveButton.addEventListener('click', () => {
+        linkShort.textContent = 'localhost/' + inputEditedUrl.value;
+        saveButton.replaceWith(editButton);
+        inputEditedUrl.replaceWith(linkShort);
+    });
+
+
+    listElement.appendChild(editButton);
+    list.appendChild(listElement);
+}
 
 function generateShortUrl() {
-    const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-    let shortUrl = "localhost/";
-    for (let i = 0; i < 5; i++) {
-        shortUrl += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return shortUrl;
+    const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return 'localhost/' + Array.from({length: 5},
+        () => charset[Math.floor(Math.random() * charset.length)])
+        .join('');
 }
 
-function isValidUrl(url) {
-    return !!/^(http(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-.\/?%:#&=]*)?$/.test(url);
+function isUrlValid(url) {
+    return /^(https?:\/\/)?(www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\S*)?$/.test(url);
 }
+
+createButton.addEventListener('click', addItem);
+urlField.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        addItem();
+    }
+});
+
+deleteButton.addEventListener('click', () => {
+    let url = urlField.value;
+    let listElements = document.querySelectorAll("li");
+
+    if (url === '') {
+        list.innerHTML = "";
+        return;
+    }
+
+    for (let i = 0; i < listElements.length; i++) {
+        if (listElements[i].childNodes[2].textContent === url) {
+            listElements[i].remove();
+        }
+    }
+})
